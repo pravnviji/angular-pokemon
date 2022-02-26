532f8d0cc02faf62e0e7c9779f11da19b9c5d44b
pravnviji <pravnviji@gmail.com>
2022-02-26 17:30:08 +0800

    updated scss

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   baseUrl: `https://pokeapi.co/api/v2/pokemon?limit=`,
   imageUrl: `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/`,
-  maxLimit: 10,
+  maxLimit: 150,
   perPageLimit: 50,
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
