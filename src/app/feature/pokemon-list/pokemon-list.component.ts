5b18f21104b58c9fcd845d836886cac028964a85
pravnviji <pravnviji@gmail.com>
2022-02-26 12:39:16 +0800

    Styles and filter added

--- src/app/feature/pokemon-list/pokemon-list.component.ts
+++ src/app/feature/pokemon-list/pokemon-list.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  fromEvent,
+  tap,
+} from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { PokemonService, TPokeMonDetails } from '../pokemon.service';
 
@@ -9,11 +15,39 @@ import { PokemonService, TPokeMonDetails } from '../pokemon.service';
   styleUrls: ['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent implements OnInit {
-  public pokemonData$: Observable<TPokeMonDetails[]> | undefined;
+  @ViewChild('searchPokemon') searchPokemon!: ElementRef;
+  public pokemonData!: TPokeMonDetails[];
+  private orginalData!: TPokeMonDetails[];
   public imageBaseUrl = environment.imageUrl;
   constructor(private pokemonService: PokemonService) {}
 
   ngOnInit(): void {
-    this.pokemonData$ = this.pokemonService.getPokemonDetails();
+    this.pokemonService
+      .getPokemonDetails()
+      .subscribe(
+        (res: TPokeMonDetails[]) => (
+          (this.orginalData = res), (this.pokemonData = this.orginalData)
+        )
+      );
+  }
+  ngAfterViewInit() {
+    fromEvent(this.searchPokemon.nativeElement, 'keyup')
+      .pipe(
+        filter(Boolean),
+        debounceTime(700),
+        distinctUntilChanged(),
+        tap((text) => {
+          console.log('Value enter ' + this.searchPokemon.nativeElement.value);
+
+          this.pokemonData = this.pokemonData.filter((data) =>
+            data.name
+              .toLowerCase()
+              .includes(this.searchPokemon.nativeElement.value)
+          );
+          if (this.searchPokemon.nativeElement.value === '')
+            this.pokemonData = this.orginalData;
+        })
+      )
+      .subscribe();
   }
 }
